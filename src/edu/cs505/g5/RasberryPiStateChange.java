c72bb61cad84080d64e9a479f67814b67f0c16c0
Madhavi <madhavimalarouth@gmail.com>
2017-10-29 20:53:09 -0400

    added java doc on the code

--- src/edu/cs505/g5/RasberryPiStateChange.java
+++ src/edu/cs505/g5/RasberryPiStateChange.java
@@ -1,7 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package edu.ccsu.cs505.g5;
-
+/**
+ *
+ * @author madhavimalarouth,vanya,Anil Kumar,Hiral Depani, Yao Wei
+ */
+/**
+ * Interface class that has the following method.
+ */
 public interface RasberryPiStateChange {
-	
-	public void setRasberryPiState();
-
+    
+    /**
+     * This method will set the state of the RasberryPi i.e whether it is in on state or in warning state or in Shutdown State
+     */
+    public void setRasberryPiState();
 }
