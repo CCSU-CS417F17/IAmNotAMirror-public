1d163d654568a094acec04388a6199b6bd2f4178
Anil kumar <31674986+anilvivek7@users.noreply.github.com>
2017-10-29 23:13:26 -0400

    Javadoc added

--- src/edu/cs505/g5/OffState.java
+++ src/edu/cs505/g5/OffState.java
@@ -2,8 +2,15 @@ package edu.ccsu.cs505.g5;
 
 import java.util.Vector;
 
+/**
+ * This class extends RaspberriPiState
+ * @author CS505-Group5 
+ */
 public class OffState extends RasberriPiState {
 
+    /**
+     * This observer sends a notification (to the user) that RaspberryPi is going to switch off
+     */
     public Vector  myObserverNotification;
   
 }
