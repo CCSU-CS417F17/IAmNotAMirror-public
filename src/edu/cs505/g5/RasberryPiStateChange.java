6fc3d2b6f2938238115d2a0199814b4486a3c179
Madhavi <madhavimalarouth@gmail.com>
2017-10-29 21:12:53 -0400

    removed information about package

--- src/edu/cs505/g5/RasberryPiStateChange.java
+++ src/edu/cs505/g5/RasberryPiStateChange.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.ccsu.cs505.g5;
 /**
  *
