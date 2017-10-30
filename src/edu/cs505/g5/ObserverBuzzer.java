3cdaa7a99f69d3421edea7ed42c27688bf0e7118
Anil kumar <31674986+anilvivek7@users.noreply.github.com>
2017-10-29 23:09:59 -0400

    Clas updated
    updated the file by adding javadoc

--- src/edu/cs505/g5/ObserverBuzzer.java
+++ src/edu/cs505/g5/ObserverBuzzer.java
@@ -1,15 +1,21 @@
 package edu.ccsu.cs505.g5;
 
-/*
+/**
+ * This class implements the interface ObserverDHTSensor
+ * Observer State is an attribute
+ * @author CS505-Group5
  */
 public class ObserverBuzzer implements ObserverDHTSensor {
 
 	public int observerState;
 
 
-	@Override
+	/**
+	 * @param subjectDHTSensor notifies on a regular interval of time
+	 */
+	
 	public void update(SubjectDHTSensor subjectDHTSensor) {
-	// TODO Auto-generated method stub
+
 	}
 
 }
