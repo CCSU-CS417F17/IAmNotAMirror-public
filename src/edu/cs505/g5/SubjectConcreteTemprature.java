25cd9c1295199eb9cf8c38e93b21399227c63d2d
Madhavi <madhavimalarouth@gmail.com>
2017-10-29 21:10:21 -0400

    added java doc on the code

--- src/edu/cs505/g5/SubjectConcreteTemprature.java
+++ src/edu/cs505/g5/SubjectConcreteTemprature.java
@@ -1,11 +1,21 @@
 package edu.ccsu.cs505.g5;
-
+/**
+ *
+ * @author CS505-Group5
+ */
+/**
+ * This class is the concrete class of SubjectDHTSensor which will be called to get and set the states of the observer
+ */
 public class SubjectConcreteTemprature extends SubjectDHTSensor {
 
-    
+    /**
+     * This method is called by concrete observer classes when they are getting updated
+     */
 	public void getState() {
 	}
-
+     /**
+      * This method is called by concrete observer classes when they have to get updated
+      */   
 	public void setState() {
 	}
 
