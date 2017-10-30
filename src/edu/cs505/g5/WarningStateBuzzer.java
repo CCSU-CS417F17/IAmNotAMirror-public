b415fe8ca85a1f28b7038ddb6e0e2d4970111392
Madhavi <madhavimalarouth@gmail.com>
2017-10-29 21:49:17 -0400

    Java doc added to the code

--- src/edu/cs505/g5/WarningStateBuzzer.java
+++ src/edu/cs505/g5/WarningStateBuzzer.java
@@ -1,8 +1,18 @@
 package edu.ccsu.cs505.g5;
 
+/**
+ *
+ * @author CS505-Group5
+ */
+
+/**
+ * This class is the state of the RasberryPi when the buzzer is active. This extends RasberriPiState
+ */
 public class WarningStateBuzzer extends RasberriPiState {
 
-	
+/**
+ * To set the state of the RasberryPi to buzzer state, an object of class ObserverBuzzer is used
+ */	
   public ObserverBuzzer observerBuzzer;
 
   
