bc58cdc55289680047ecaeef18f244b77d1b9efb
Madhavi <madhavimalarouth@gmail.com>
2017-11-05 18:20:33 -0500

    test case body is added

---
+++ Test/SubjectSensorTest.java
@@ -0,0 +1,19 @@
+package edu.ccsu.cs505.compob;
+
+
+
+class SubjectSensorTest {
+
+    @Test
+    void testaddObserver() throws IOException, InterruptedException, Exception {
+     
+    }
+
+    @Test
+    void testremoveObserver() throws IOException, InterruptedException, Exception{
+
+    
+
+    }
+
+}
