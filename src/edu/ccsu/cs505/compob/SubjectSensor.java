1c9d78a42486c3b49f5e531e5da166953d88eb58
Anil kumar <31674986+anilvivek7@users.noreply.github.com>
2017-11-05 17:01:00 -0500

    Updated javadoc

--- src/edu/ccsu/cs505/compob/SubjectSensor.java
+++ src/edu/ccsu/cs505/compob/SubjectSensor.java
@@ -3,23 +3,14 @@ package edu.ccsu.cs505.compob;
 import java.util.*;
 
 /**
- *
  * @author CS505-Group5
+ * This an abstract class and it is Subject class in the observer pattern which will notify the observers if the temperature goes beyond the limit-set
  */
 
-
-/**
- * This class is the Subject class in the observer pattern which will notify the observers if the temperature goes beyond the limit-set
- */
 public abstract class SubjectSensor {
 
-	
-    /**
-     * Because the SubjectDHTSensor calls the interface Observer and all other classes that implement that interface as well
-     */
-
 	/**
-	 * this class notifies the observer classes through the object observerDHTSensor
+	 * This attribute notifies the observer classes through the object observerDHTSensor
 	 */
 	public List <ObserverSensor> observers = new ArrayList<>();
 
@@ -48,3 +39,5 @@ public abstract class SubjectSensor {
 	}
 
 }
+
+ 
