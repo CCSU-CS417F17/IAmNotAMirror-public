4a60743cda1b6e7f4ef2c50609aa22f05553082d
hiraldepani <31705206+hiraldepani@users.noreply.github.com>
2017-11-05 22:19:22 -0500

    Updated Code

--- src/edu/ccsu/cs505/compob/SubjectDHTSensor.java
+++ src/edu/ccsu/cs505/compob/SubjectDHTSensor.java
@@ -2,83 +2,144 @@ package edu.ccsu.cs505.compob;
 
 import java.util.*;
 import java.util.TimerTask;
+import java.io.IOException;
 import com.dexterind.grovepi.sensors.*;
 
 
 public class SubjectDHTSensor extends SubjectSensor {
-	
-	public List <ObserverSensor> observers = new ArrayList<>();
-	
-	private double interval;
-	private double temperature;
-	private double humidity;
-	private int scale = 0;
-	private DHTDigitalSensor dhtSensor;
-	
 
-	public SubjectDHTSensor() {
-		super();
-		this.dhtSensor = new DHTDigitalSensor(1, 0, this.scale);
-		startTimer();
-	}
-	
-	public SubjectDHTSensor(double interval) {
-		super();
-		this.interval = interval;
-		this.dhtSensor = new DHTDigitalSensor(1, 0, this.scale);
-		startTimer();
-	}
-	
-	public SubjectDHTSensor(int scale, double interval) {
-		super();
-		this.scale = scale;
-		this.interval = interval;
-		this.dhtSensor = new DHTDigitalSensor(1, 0, this.scale);
-		startTimer();
-	}
-	
-	private void startTimer() {
-		Timer timer = new Timer();
-		
-		timer.schedule(new TimerTask() {
-			public void run() {
-				read();
-				notifyObservers();
-			}
-		}, (long) interval);
-	}
+    /**
+     * Represents the interval time, scale is second.
+     */
+    private double interval = 0;
 
-	
-	/**
-     * Read temperature and humidity from 
-     * DHTDigitalSensor's read function
-     */
-	private void read() {
-		float data[] = this.dhtSensor.read();
-		this.temperature = data[0];
-		this.humidity = data[1];
-	}
-	
     /**
-     * This method is called by concrete observer classes when they are getting updated
+     * Represents the temperature data.
      */
-	public double getTemperaturee() {
-		return this.temperature;
-	}
-	
+    private double temperature;
+
     /**
-     * This method is called by concrete observer classes when they are getting updated
+     * Represents the humidity data.
      */
-	public double getHumidity() {
-		return this.humidity;
-	}
-	
+    private double humidity;
+
     /**
-     * This method is called by concrete observer classes when they are getting updated
+     * Represents the scale of temperature, 0 is C, and 1 is F.
      */
-	public double getInterval() {
-		return this.interval;
-	}
-	
-	
+    private int scale = 0;
+
+    /**
+     * Represents the DHT Sensor class.
+     */
+    private final DHTDigitalSensor dhtSensor;
+
+    /**
+     * Represents the pin number of DHT sensor on the Grove Pi, default value is
+     * 3.
+     */
+    private int pinNum = 3;
+
+    public SubjectDHTSensor() throws IOException, InterruptedException, Exception {
+        super();
+        this.dhtSensor = new DHTDigitalSensor(this.pinNum, 0, this.scale);
+        startTimer();
+    }
+
+    public SubjectDHTSensor(double interval) throws IOException, InterruptedException, Exception {
+        super();
+        this.interval = interval;
+        this.dhtSensor = new DHTDigitalSensor(this.pinNum, 0, this.scale);
+        startTimer();
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param scale, scale of temperature.
+     * @param interval, interval time, by seconds.
+     * @throws java.io.IOException
+     * @throws java.lang.InterruptedException
+     *
+     */
+    public SubjectDHTSensor(int scale, double interval) throws IOException, InterruptedException, Exception {
+        super();
+        this.scale = scale;
+        this.interval = interval;
+        this.dhtSensor = new DHTDigitalSensor(this.pinNum, 0, this.scale);
+        startTimer();
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param scale, scale of temperature.
+     * @param interval, interval time, by seconds.
+     * @param pinNum , Pin number.
+     * @throws java.io.IOException
+     * @throws java.lang.InterruptedException
+     *
+     */
+    public SubjectDHTSensor(int scale, double interval, int pinNum) throws IOException, InterruptedException, Exception {
+        super();
+        this.scale = scale;
+        this.interval = interval;
+        this.pinNum = pinNum;
+        this.dhtSensor = new DHTDigitalSensor(this.pinNum, 0, this.scale);
+        startTimer();
+    }
+
+    /**
+     * Once the subject initialized, it will run by interval time, the Java
+     * default timer schedule function is running by interval, the unit is
+     * millisecond, so we have to * 1000 to get seconds.
+     */
+    private void startTimer() {
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                read();
+                notifyObservers();
+            }
+        }, (long) interval * 1000, (long) interval * 1000);
+    }
+
+    /**
+     * Read temperature and humidity from DHTDigitalSensor's read function
+     */
+    private void read() {
+        float data[] = this.dhtSensor.read();
+        this.temperature = data[0];
+        this.humidity = data[1];
+        System.out.println("temperature:"+temperature+"  humidity:"+humidity);
+    }
+
+    /**
+     * Gets temperature data.
+     *
+     * @return temperature
+     */
+    public double getTemperature() {
+        return this.temperature;
+    }
+
+    /**
+     * Gets humidity data.
+     *
+     * @return humidity
+     */
+    public double getHumidity() {
+        return this.humidity;
+    }
+
+    /**
+     * Gets interval time(seconds).
+     *
+     * @return interval
+     */
+    public double getInterval() {
+        return this.interval;
+    }
+
 }
