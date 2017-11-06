cb3eca01d08472e79505dcd73fc424968635f642
Anil kumar <31674986+anilvivek7@users.noreply.github.com>
2017-11-05 20:38:29 -0500

    Updated javadoc

--- src/edu/ccsu/cs505/compob/LEDObserverWarner.java
+++ src/edu/ccsu/cs505/compob/LEDObserverWarner.java
@@ -1,19 +1,28 @@
 package edu.ccsu.cs505.compob;
 
 /**
- * This class also implements the interface ObserverDHTSensor
- * Observer State is an attribute
  * @author CS505-Group5
+ * This class is a child class of TempObserverWarner
  */
-public class LEDObserverWarner extends TempObserverWarner {
 
-	//used to define warning temperature  
+public class LEDObserverWarner extends TempObserverWarner {
+  
+	/**
+	 * This attribute is for warning, whenever it exceeds the threshold of temperature 
+	 */
 	private double warnTemp;
 	
-	//used to define warning state, 1 means on warning, 0 means not.
+	/**
+	 * This attribute is used for warning state with boolean function, wherein 1 stands for warning, 0 stands for normal
+	 */
 	private boolean warnState;
 	
 
+	/**
+	 * This Attribute is for the LED to lit up after receiving the warning
+	 * @param sub is for the subject
+	 * @param temp is for temperature warning
+	 */
 	public LEDObserverWarner (SubjectDHTSensor sub, double temp) {
 		super();
 		this.subject = sub;
@@ -23,17 +32,20 @@ public class LEDObserverWarner extends TempObserverWarner {
 
 	
 	/**
-	 * @param subjectDHTSensor notifies on a regular interval of time
+	 * @param subjectDHTSensor notifies on a regular interval of time about any fluctuation in temperature
 	 */
-	@Override
 	public void update() {
-		//If not warning
+	/**
+	* There will be warning once it exceeds the threshold of the temperature
+	*/
 				if (!this.warnState) {
 					if (this.subject.getTemperaturee() > this.warnTemp) {
 						warn();
 					}
 				}
-				//if it has been warning.
+	/**
+	* There is no need of warning when the temperature doesn't exceed the threshold
+	*/
 				else {
 					if (this.subject.getTemperaturee() <= this.warnTemp) {
 						warnfree();
@@ -44,26 +56,30 @@ public class LEDObserverWarner extends TempObserverWarner {
 	
 	
 	/**
-     * Warn by flashing LED, according to temperature from update function.
-     * 
+     * This method will generate warning by flashing LED, according to temperature from update function.
      */
 	private void warn() {
 		System.out.println("start warning");
 	}
 	
 	/**
-     * Release warning LED, according to temperature from update function.
-     * 
+     * This method will not generate warning, according to temperature from update function.
      */
 	private void warnfree() {
 		System.out.println("no warning");
 	}
 	
 	
+	/**
+	 * @param temp is to set the update for temperature, so that warning can be generated, when required.
+	 */
 	public void setWarnTemp (double temp) {
 		this.warnTemp = temp;
 	}
 	
+	/**
+	 * @return is to get the update for temperature, and warning will be generated accordingly.
+	 */
 	public double getWarnTemp () {
 		return this.warnTemp;
 	}
@@ -73,3 +89,4 @@ public class LEDObserverWarner extends TempObserverWarner {
 
 	
 }
+
